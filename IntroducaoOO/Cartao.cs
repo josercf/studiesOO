f28934c15f4c1e9a4e4bd4b12f7f28bf459ef246
José Romualdo <jromualdo@iaudit.com.br>
2019-04-03 12:22:34 -0300

    Add project files.

---
+++ IntroducaoOO/Cartao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntroducaoOO
+{
+     public class Cartao
+    {
+        //Propriedade
+        public string Numero { get;private set; }
+
+        public bool AutorizarCompra(string senha)
+        {
+            if (senha == "123")
+                return true;
+            else
+                return false;
+        }
+
+        // Modificadores de Acesso
+        //private      - Acesso apenas no escopo da classe
+        //internal     - Apenas dentro do pacote
+        //protected    - Apenas para os herdeiros
+        //public       - Visivel a todos
+    }
+}
+
+
+
---
+++ IntroducaoOO/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace IntroducaoOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cartao cartao = new Cartao();
+
+            bool autorizado = cartao.AutorizarCompra("jose");
+
+            if (autorizado == true)
+            {
+                Console.WriteLine("Compra autorizada");
+            }
+            else
+            {
+                Console.WriteLine("Compra negada");
+            }
+
+
+
+
+
+            Console.WriteLine(cartao.Numero);
+
+            Console.ReadKey();
+        }
+    }
+}
